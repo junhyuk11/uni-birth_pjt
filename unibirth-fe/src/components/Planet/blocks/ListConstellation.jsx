379cae596c67f7ef49182dbc3b842927355bd500
seobstation <ljs8828@naver.com>
2023-08-11 22:27:49 +0900

    움직임 방지

--- unibirth-fe/src/components/Planet/blocks/ListConstellation.jsx
+++ unibirth-fe/src/components/Planet/blocks/ListConstellation.jsx
@@ -24,58 +24,63 @@ import Plus from "../../../assets/icons/js/plus";
 
 // R3F 훅 카메라 컨트롤러 컴포넌트
 function CameraController({ ConstellationPosition, zoomed }) {
-  console.log("콘솔로그::", ConstellationPosition);
-  console.log("캐캐캐", zoomed);
-
-  // 현재 별자리 리스트 리코일 관리
+  const [animating, setAnimating] = useState(false);
 
   const { camera } = useThree();
   const cameraRef = useRef(camera);
   cameraRef.current = camera;
   const zoomFactor = 0.1;
   const multiFactor = 1.5;
-  console.log("ConstellationPosition:", ConstellationPosition);
+  // console.log("ConstellationPosition:", ConstellationPosition);
+  useEffect(() => {
+    if (ConstellationPosition) {
+      // console.log("제[발!!ㅣ", ConstellationPosition);
+      const targetPosition = zoomed
+        ? {
+            x: ConstellationPosition.x * multiFactor + 2000,
+            y: ConstellationPosition.y * multiFactor + 2000,
+            z: ConstellationPosition.z * multiFactor,
+          }
+        : {
+            x: ConstellationPosition.x * zoomFactor,
+            y: ConstellationPosition.y * zoomFactor,
+            z: ConstellationPosition.z * zoomFactor,
+          };
+      const startPosition = {
+        x: camera.position.x,
+        y: camera.position.y,
+        z: camera.position.z,
+      };
 
-  if (ConstellationPosition) {
-    console.log("제[발!!ㅣ", ConstellationPosition);
-    const targetPosition = zoomed
-      ? {
-          x: ConstellationPosition.x * multiFactor + 2000,
-          y: ConstellationPosition.y * multiFactor + 2000,
-          z: ConstellationPosition.z * multiFactor,
-        }
-      : {
-          x: ConstellationPosition.x * zoomFactor,
-          y: ConstellationPosition.y * zoomFactor,
-          z: ConstellationPosition.z * zoomFactor,
-        };
-    const startPosition = {
-      x: camera.position.x,
-      y: camera.position.y,
-      z: camera.position.z,
-    };
+      console.log("카메라 표지션:", startPosition);
 
-    console.log("카메라 표지션:ㅣ", startPosition);
+      const updateCameraPosition = () => {
+        cameraRef.current.position.set(
+          startPosition.x,
+          startPosition.y,
+          startPosition.z,
+        );
+        cameraRef.current.lookAt(0, 0, 0);
+        cameraRef.current.updateProjectionMatrix();
+      };
 
-    const updateCameraPosition = () => {
-      cameraRef.current.position.set(
-        startPosition.x,
-        startPosition.y,
-        startPosition.z,
-      );
-      cameraRef.current.lookAt(0, 0, 0);
-      cameraRef.current.updateProjectionMatrix();
-    };
-
-    gsap.to(startPosition, {
-      duration: 2, // duration in seconds
-      x: -targetPosition.x,
-      y: -targetPosition.y,
-      z: -targetPosition.z,
-      onUpdate: updateCameraPosition,
-      // ease: "power1.inOut", // easing function for smooth transition
-    });
-  }
+      gsap.to(startPosition, {
+        duration: 1.5, // duration in seconds
+        x: -targetPosition.x,
+        y: -targetPosition.y,
+        z: -targetPosition.z,
+        onStart: () => setAnimating(true),
+        onUpdate: updateCameraPosition,
+        ease: "Power1.inOut",
+        onComplete: () => {
+          setAnimating(false);
+        },
+      });
+    }
+  }, [ConstellationPosition]);
+  useEffect(() => {
+    document.body.style.pointerEvents = animating ? "none" : "auto";
+  }, [animating]);
   return null;
 }
 
