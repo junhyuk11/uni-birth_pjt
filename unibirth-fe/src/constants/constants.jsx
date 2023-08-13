578dbec3a14b60b54edc788aae9f131586f61a32
seobstation <ljs8828@naver.com>
2023-08-13 19:22:33 +0900

    dropdown 완성

--- unibirth-fe/src/constants/constants.jsx
+++ unibirth-fe/src/constants/constants.jsx
@@ -1,5 +1,6 @@
 // API URL
-export const API_URL = "http://43.202.47.103:8080";
+// export const API_URL = "http://43.202.47.103:8080";
+export const API_URL = "https://i9a410.p.ssafy.io/api/";
 
 export const CONTENT_TYPE_JSON = "application/json";
 
