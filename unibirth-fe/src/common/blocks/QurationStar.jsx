4aca860d595eeb5624939bb5e543437f4d3da6ac
kimmainsain <kmsid003@gmail.com>
2023-08-13 12:30:35 +0900

    수정 : curation 디자인 개선

--- unibirth-fe/src/common/blocks/QurationStar.jsx
+++ unibirth-fe/src/common/blocks/QurationStar.jsx
@@ -7,19 +7,31 @@ const QurationStar = ({ data }) => {
 
   return (
     <div className="flex w-full flex-col items-center justify-center">
+      {/* 이미지 */}
       <div className="w-full" onClick={() => navigateToDetailStar(data.starId)}>
         <img
           src={data.imageUrl}
           alt="별 이미지"
           className="h-auto max-h-96 w-full object-cover"
-        />{" "}
+        />
       </div>
-      <h3 className="font-bold">{data.nickname}</h3>
-      <p>{data.content}</p>
-      <Button1
-        value="별 보기"
-        onClick={() => navigateToDetailStar(data.starId)}
-      />
+
+      {/* 닉네임 */}
+      <div className="mt-2 w-full text-center font-bold">{data.nickname}</div>
+
+      {/* 제목, 내용 및 버튼 */}
+      <div className="mt-2 flex w-full items-center justify-between px-4">
+        <div className="flex flex-col">
+          <div className="font-bold">data.title 이거 나중에 괄호치면 댐</div>
+          <div>{data.content}</div>
+        </div>
+        <div>
+          <Button1
+            value="별 보기"
+            onClick={() => navigateToDetailStar(data.starId)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
