bc5fbd7056fe761da06411d8e7e99519dee47af5
kimmainsain <kmsid003@gmail.com>
2023-08-20 23:37:43 +0900

    수정 : 큐레이션 별자리버튼 생성, 검색 빈값일경우 에러alert
    Former-commit-id: 9856dca920c28c70595a142fc11555285540c8bd

--- unibirth-fe/src/common/blocks/QurationStar.jsx
+++ unibirth-fe/src/common/blocks/QurationStar.jsx
@@ -3,7 +3,8 @@ import Button1 from "../atoms/Button1";
 import { useNavigation } from "../../hooks/useNavigation";
 
 const QurationStar = ({ data }) => {
-  const { navigateToDetailStar } = useNavigation();
+  const { navigateToDetailStar, navigateToDetailConstellation } =
+    useNavigation();
 
   return (
     <div className="flex w-full flex-col items-center justify-center">
@@ -19,18 +20,26 @@ const QurationStar = ({ data }) => {
         />
       </div>
 
-      <div className="mb-2 w-full px-4 text-xl font-bold">{data.title}</div>
+      <div className="mb-2 flex w-full items-center justify-between px-4">
+        <div className="text-xl font-bold">{data.title}</div>
+        <div className="flex">
+          <Button1
+            value="별 보기"
+            className="mr-2 text-base font-normal"
+            onClick={() => navigateToDetailStar(data.starId)}
+          />
+          <Button1
+            value="별 자리"
+            className="text-base font-normal"
+            onClick={() => navigateToDetailConstellation(data.constellationId)}
+          />
+        </div>
+      </div>
 
       {/* 닉네임, 내용 및 버튼 */}
       <div className="mb-4 flex w-full justify-between px-4">
         <div className="text-gray-300">{data.content}</div>
-        <div>
-          <div className="mb-2">{data.nickname}</div>
-          <Button1
-            value="별 보기"
-            onClick={() => navigateToDetailStar(data.starId)}
-          />
-        </div>
+        <div className="mb-2 ml-4 font-bold">{data.nickname}</div>
       </div>
     </div>
   );
